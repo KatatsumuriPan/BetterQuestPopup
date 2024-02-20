a837ceb1114d674781a59f631c6bd8304e22b500
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-02-20 17:00:26 +0900

    1.12.2の開発はデフォルトブランチから1.12.2forgeへ移動しました

--- src/main/java/kpan/bq_popup/asm/hook/HK_ClientQuestFile.java
+++
@@ -1,9 +0,0 @@
-package kpan.bq_popup.asm.hook;
-
-import kpan.bq_popup.client.DisplayedPopup;
-
-public class HK_ClientQuestFile {
-	public static void onLoad() {
-		DisplayedPopup.onLoad();
-	}
-}
--- src/main/java/kpan/bq_popup/proxy/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package kpan.bq_popup.proxy;
-
-@SuppressWarnings("unused")
-public class CommonProxy {
-	public void registerOnlyClient() { }
-
-	public boolean hasClientSide() { return false; }
-}
