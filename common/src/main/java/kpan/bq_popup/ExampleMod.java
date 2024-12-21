dc194f5a6758af3f54741ba644699b6524fa0891
KatatsumuriPan <katatsumuri.pan@gmail.com>
2024-12-21 17:17:03 +0900

    Initial commit

---
+++ common/src/main/java/kpan/bq_popup/ExampleMod.java
@@ -0,0 +1,9 @@
+package kpan.bq_popup;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "better_quest_popup";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/kpan/bq_popup/fabric/ExampleModFabric.java
@@ -0,0 +1,17 @@
+package kpan.bq_popup.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import kpan.bq_popup.ExampleMod;
+
+public final class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
